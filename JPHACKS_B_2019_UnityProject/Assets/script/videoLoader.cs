f49262402e9b6053805fa217fc22d3ed7edb2ca1
Kyoichi <40378788+kyoichi001@users.noreply.github.com>
2020-10-31 17:07:56 +0900

    mp4 loader

--- JPHACKS_B_2019_UnityProject/Assets/script/videoLoader.cs
+++ JPHACKS_B_2019_UnityProject/Assets/script/videoLoader.cs
@@ -1,19 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
+using B83.Win32;
 
 public class videoLoader : MonoBehaviour
 {
     public int s = 0;
+    [SerializeField]VideoPlayer player;
     // Start is called before the first frame update
-    void Start()
+    void OnEnable()
     {
-        
+        FileDragAndDrop.Instance.AddOnFiles(OnFiles);
+    }
+    void OnFiles(List<string> aFiles, POINT aPos)
+    {
+        player.Stop();
+        foreach (var file in aFiles)
+        {
+            GenerateFromPath(file);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    //https://qiita.com/seka/items/4197e97562b1f071b8af
+    public void GenerateFromPath(string path)
     {
-        
+        player.url = path;
+        player.Play();
+
     }
+
 }
