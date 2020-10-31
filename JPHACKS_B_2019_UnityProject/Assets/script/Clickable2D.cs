863a2b0f899609303b25887fa78c794945f25e7c
Kyoichi <40378788+kyoichi001@users.noreply.github.com>
2020-10-31 16:48:39 +0900

    ray cast 3D

---
+++ JPHACKS_B_2019_UnityProject/Assets/script/Clickable2D.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//==================================================================
+//うまくうごきません
+//
+
+public class Clickable2D : MonoBehaviour
+{
+    Color defaultColor;
+    Camera mainCamera;
+    Renderer renderer;
+    [SerializeField]Collider2D collider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        collider = GetComponent<Collider2D>();
+        mainCamera = Camera.main;
+        defaultColor = renderer.material.GetColor("_Color");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        var info = Physics2D.Raycast(ray.origin, ray.direction);
+        if (info.collider == collider)
+        {
+            renderer.material.SetColor("_Color", defaultColor * new Color(0.5f, 0.5f, 0.5f, 1.0f));
+        }
+        else
+        {
+            renderer.material.SetColor("_Color", defaultColor);
+        }
+
+    }
+}
---
+++ JPHACKS_B_2019_UnityProject/Assets/script/Clickable3D.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clickable3D : MonoBehaviour
+{
+    Color defaultColor;
+    Camera mainCamera;
+    Renderer renderer;
+    Collider collider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        collider = GetComponent<Collider>();
+        mainCamera = Camera.main;
+        defaultColor = renderer.material.GetColor("_Color");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit_info = new RaycastHit();
+        float max_distance = 100f;
+
+        bool is_hit = Physics.Raycast(ray, out hit_info, max_distance);
+
+        if (is_hit && hit_info.collider == collider)
+        {
+            renderer.material.SetColor("_Color", defaultColor * new Color(0.5f, 0.5f, 0.5f, 1.0f));
+        }
+        else
+        {
+            renderer.material.SetColor("_Color", defaultColor);
+        }
+
+    }
+}
